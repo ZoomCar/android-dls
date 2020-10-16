34442fdc6afcd29cbd1592ffbeab791f44a7c31b
Shishir Dwivedi <shishirdwivedi5@gmail.com>
2020-10-16 16:44:35 +0530

    added icon in selection item data

--- zoomdls/src/main/java/com/zoomcar/uikit/optionsItem/ZSelectionItemCell.kt
+++ zoomdls/src/main/java/com/zoomcar/uikit/optionsItem/ZSelectionItemCell.kt
@@ -10,7 +10,9 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.core.widget.TextViewCompat
 import androidx.databinding.DataBindingUtil
+import com.zoomcar.util.getNullCheck
 import com.zoomcar.util.isValid
+import com.zoomcar.util.loadImage
 import com.zoomcar.zoomdls.R
 import com.zoomcar.zoomdls.databinding.LayoutSelectionItemBinding
 import kotlinx.android.parcel.Parcelize
@@ -42,9 +44,21 @@ class ZSelectionItemCell : ConstraintLayout, View.OnClickListener {
             isVisible = model.isEditable
             setOnClickListener(this@ZSelectionItemCell)
         }
+
+        when {
+            model.imageRes.getNullCheck() -> {
+                binding.imageType.setImageResource(model.imageRes!!)
+            }
+            model.imageUrl.isValid() -> {
+                binding.imageType.loadImage(model.imageUrl)
+            }
+            else -> {
+                binding.imageType.setImageDrawable(null)
+            }
+        }
+
         when (model.type) {
             SelectionItemType.PRIMARY -> {
-                binding.imageType.setImageResource(R.drawable.ic_z_circle_grey)
                 binding.textTitle.apply {
                     TextViewCompat.setTextAppearance(this, R.style.OverlineSecondary)
                 }
@@ -53,7 +67,6 @@ class ZSelectionItemCell : ConstraintLayout, View.OnClickListener {
                 }
             }
             SelectionItemType.HIGHLIGHTED -> {
-                binding.imageType.setImageResource(R.drawable.ic_z_circle_green)
                 binding.textTitle.apply {
                     TextViewCompat.setTextAppearance(this, R.style.Button2Primary)
                 }
@@ -62,7 +75,6 @@ class ZSelectionItemCell : ConstraintLayout, View.OnClickListener {
                 }
             }
             SelectionItemType.DEFAULT -> {
-                binding.imageType.setImageResource(R.drawable.ic_z_circle_grey)
                 binding.textTitle.apply {
                     TextViewCompat.setTextAppearance(this, R.style.Body2Primary)
                 }
@@ -94,6 +106,8 @@ class ZSelectionItemCell : ConstraintLayout, View.OnClickListener {
     @Parcelize
     data class SelectionItemUIModel(
             var id: String,
+            var imageRes: Int?,
+            var imageUrl: String? = null,
             var title: String? = null,
             var desc: String? = null,
             var isEditable: Boolean = false,
