3275407da44585a87bd81ca7e2b844ec18edbf1d
Gideon Paul <gideon.paul@zoomcar.com>
2021-10-07 14:34:05 +0530

    Create PerformanceMeter.kt

---
+++ zoomdls/src/main/java/com/zoomcar/uikit/driverscore/PerformanceMeter.kt
@@ -0,0 +1,15 @@
+package com.zoomcar.uikit.driverscore
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Originally authored by Shishir.
+ * Modified by Gideon Paul.
+ */
+class PerformanceMeter @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+
+}
