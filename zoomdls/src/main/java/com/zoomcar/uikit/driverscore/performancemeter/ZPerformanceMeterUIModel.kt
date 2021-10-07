037bd8536fa12ac6eb8939f70ac0f6abbe676ae4
Gideon Paul <gideon.paul@zoomcar.com>
2021-10-07 19:15:01 +0530

    A variable to indicate if pointer should be tinted or not.

--- zoomdls/src/main/java/com/zoomcar/uikit/driverscore/performancemeter/ZPerformanceMeterUIModel.kt
+++ zoomdls/src/main/java/com/zoomcar/uikit/driverscore/performancemeter/ZPerformanceMeterUIModel.kt
@@ -7,5 +7,6 @@ data class ZPerformanceMeterUIModel(
     val rankScales: List<RankScale> = RankScale.defaultRankScales,
     val score: Int,
     @DrawableRes val pointerDrawableRes: Int? = R.drawable.ic_chevron_down,
+    val tintPointer: Boolean = true,
     val category: ScoreCategoryType = ScoreCategoryType.UNKNOWN,
 )
