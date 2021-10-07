cbc94bd9fadb69c26dd198b20ec12d6859e782bb
Gideon Paul <gideon.paul@zoomcar.com>
2021-10-07 18:10:11 +0530

    RankScale will be holding color data too.

--- zoomdls/src/main/java/com/zoomcar/uikit/driverscore/performancemeter/RankScale.kt
+++ zoomdls/src/main/java/com/zoomcar/uikit/driverscore/performancemeter/RankScale.kt
@@ -1,8 +1,12 @@
 package com.zoomcar.uikit.driverscore.performancemeter
 
+import androidx.annotation.ColorRes
+import com.zoomcar.zoomdls.R
+
 data class RankScale(
     var low: Int? = null,
     var high: Int? = null,
+    @ColorRes var color: Int? = null,
     var category: String? = null,
     var text: String? = null,
 ) {
@@ -11,16 +15,19 @@ data class RankScale(
             RankScale(
                 low = 0,
                 high = 20,
+                color = R.color.fire_red_06,
                 category = ScoreCategoryType.BAD.category
             ),
             RankScale(
                 low = 20,
                 high = 80,
+                color = R.color.sunrise_yellow_04,
                 category = ScoreCategoryType.AVERAGE.category
             ),
             RankScale(
                 low = 80,
                 high = 100,
+                color = R.color.ever_green_06,
                 category = ScoreCategoryType.GOOD.category
             )
         )
