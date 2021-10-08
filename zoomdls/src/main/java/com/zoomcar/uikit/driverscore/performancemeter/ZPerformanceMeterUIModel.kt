3570a168f833316bae5fdc1bc98b8c803fdff360
Gideon Paul <gideon.paul@zoomcar.com>
2021-10-08 12:00:48 +0530

    Removed score pointer logic from ZPerformanceMeterUIModel.

--- zoomdls/src/main/java/com/zoomcar/uikit/driverscore/performancemeter/ZPerformanceMeterUIModel.kt
+++ zoomdls/src/main/java/com/zoomcar/uikit/driverscore/performancemeter/ZPerformanceMeterUIModel.kt
@@ -1,19 +1,12 @@
 package com.zoomcar.uikit.driverscore.performancemeter
 
-import androidx.annotation.DrawableRes
-import com.zoomcar.zoomdls.R
-
 /**
  * UI model for performance meter.
  *
  * @property rankScales
  * @property score Score to be indicated on performance meter.
- * @property pointerDrawableRes Drawable for pointer to be displayed on meter.
- * @property tintPointer Indicates if the pointer should be tinted.
  */
 data class ZPerformanceMeterUIModel(
     val rankScales: List<RankScale> = RankScale.defaultRankScales,
     val score: Int,
-    @DrawableRes val pointerDrawableRes: Int? = R.drawable.ic_chevron_down,
-    val tintPointer: Boolean = true,
 )
