5e06ce494591068e4d989a34e6756633e1cf2062
rohilshahZC <82877242+rohilshahZC@users.noreply.github.com>
2022-05-09 18:48:01 +0530

    remove new segment control

--- zoomdls/src/main/java/com/zoomcar/uikit/segmentedcontrol/ZSegmentedControl.kt
+++ zoomdls/src/main/java/com/zoomcar/uikit/segmentedcontrol/ZSegmentedControl.kt
@@ -1,29 +1,38 @@
 package com.zoomcar.uikit.segmentedcontrol
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.annotation.AttrRes
 import androidx.annotation.DimenRes
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.zoomcar.util.UiUtil
 import com.zoomcar.util.isValid
 import com.zoomcar.zoomdls.R
 import com.zoomcar.zoomdls.databinding.LayoutSegmentedControlBinding
+import kotlinx.android.parcel.Parcelize
 
-class ZSegmentedControl : ConstraintLayout, ZSegmentedControlAdapter.IZSegmentedButtonClickListener {
+class ZSegmentedControl : ConstraintLayout,
+    ZSegmentedControlAdapter.IZSegmentedButtonClickListener {
 
     constructor(context: Context) : super(context)
 
-    constructor(context: Context,
-                attrs: AttributeSet?
+    constructor(
+        context: Context,
+        attrs: AttributeSet?
     ) : super(context, attrs)
 
-    constructor(context: Context,
-                attrs: AttributeSet? = null,
-                @AttrRes defStyleAttr: Int = 0
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        @AttrRes defStyleAttr: Int = 0
     ) : super(context, attrs, defStyleAttr)
 
     var model: ZSegmentedControlUIModel? = null
@@ -42,7 +51,8 @@ class ZSegmentedControl : ConstraintLayout, ZSegmentedControlAdapter.IZSegmented
 
     private fun initRecylerView() {
         binding.segmentedControl.apply {
-            layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, false)
+            layoutManager =
+                LinearLayoutManager(binding.root.context, LinearLayoutManager.HORIZONTAL, false)
             adapter = segmentAdapter
             addItemDecoration(SegmentedControlItemDecorator(context))
         }
@@ -50,8 +60,8 @@ class ZSegmentedControl : ConstraintLayout, ZSegmentedControlAdapter.IZSegmented
 
     fun setMargin(@DimenRes margin: Int) {
         val params = LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.WRAP_CONTENT
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.WRAP_CONTENT
         )
         val marginInPix = resources.getDimensionPixelSize(margin)
         params.setMargins(marginInPix, marginInPix, marginInPix, marginInPix)
@@ -60,8 +70,8 @@ class ZSegmentedControl : ConstraintLayout, ZSegmentedControlAdapter.IZSegmented
 
     fun seTopMargin(@DimenRes margin: Int) {
         val params = LayoutParams(
-                LayoutParams.MATCH_PARENT,
-                LayoutParams.WRAP_CONTENT
+            LayoutParams.MATCH_PARENT,
+            LayoutParams.WRAP_CONTENT
         )
         val marginInPix = resources.getDimensionPixelSize(margin)
         params.setMargins(0, marginInPix, 0, 0)
@@ -75,8 +85,25 @@ class ZSegmentedControl : ConstraintLayout, ZSegmentedControlAdapter.IZSegmented
             isVisible = model?.header.isValid()
             measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
         }
+
+        when (item.type) {
+            SegmentControlType.DEFAULT -> {
+                setPadding(4, 4, 4, 4)
+                background =
+                    ContextCompat.getDrawable(context, R.drawable.segmented_control_layout_bg)
+            }
+            SegmentControlType.WITH_IMAGE -> {
+                binding.segmentedControl.apply {
+                    setPadding(0, 0, 0, 0)
+                    background = ContextCompat.getDrawable(
+                        context,
+                        R.drawable.segmented_control_layout_bg_radius_2_7
+                    )
+                }
+            }
+        }
         model?.list?.let {
-            segmentAdapter.setData(it)
+            segmentAdapter.setData(it, item.type)
         }
     }
 
@@ -96,15 +123,18 @@ class ZSegmentedControl : ConstraintLayout, ZSegmentedControlAdapter.IZSegmented
     private fun calculateCellSize() {
         val width = if (binding.textHeader.isVisible) binding.textHeader.measuredWidth else 0
         val recyclerSize = parentWidth - width
-        val itemCellSize = (recyclerSize / (model?.list?.size ?: 1).toFloat()).toInt()
+        val itemCellSize = ((recyclerSize - 2 * UiUtil.dpToPixels(4, context)) / (model?.list?.size
+            ?: 1).toFloat()).toInt()
         segmentAdapter.setCellSize(itemCellSize)
     }
 
+    @Parcelize
     data class ZSegmentedControlUIModel(
-            val id: String,
-            val header: String,
-            val list: List<ZSegmentedControlAdapter.ZSegmentedControlButtonModel>
-    )
+        val type: SegmentControlType = SegmentControlType.DEFAULT,
+        val id: String,
+        val header: String,
+        val list: List<ZSegmentedControlAdapter.ZSegmentedControlButtonModel>
+    ) : Parcelable
 
     interface IZSegmentedControlListener {
         fun onSegmentSelected(headerId: String, filterId: String)
@@ -114,5 +144,10 @@ class ZSegmentedControl : ConstraintLayout, ZSegmentedControlAdapter.IZSegmented
         model?.let { data ->
             listener?.onSegmentSelected(data.id, data.list[position].id)
         }
+    }
+
+    enum class SegmentControlType {
+        DEFAULT,
+        WITH_IMAGE;
     }
 }
